7704d3847e740571735fa0d313628d7440f65b1f
jeffsedrel <jeffsedrel@gmail.com>
2022-05-02 20:26:45 -0500

    Assignment1_1

---
+++ Unit-1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Jeff Sedrel");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Ice Cream");
+    }
+}
